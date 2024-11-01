ea40be196ec1baa11f98c7cb9db2df1f13ce4ff4
Ahmed <brazfu145@gmail.com>
2024-11-01 15:08:13 +0100

    factorial

--- factorial.js
+++ factorial.js
@@ -6,3 +6,16 @@ console.log(factorial(5));
 
 // output 
 // 120
+
+// ******************************************* //
+
+function factorialRecursive(n) {
+    if (n < 0) return undefined;
+    if (n === 0 || n === 1) return 1;
+    return n * factorialRecursive(n - 1);
+}
+
+console.log(factorialRecursive(5));
+
+// output 
+// 120
--- sum.js
+++ sum.js
@@ -1,9 +1,8 @@
-const arr = [9, 12, 34, 24, 130];
 function sum(arr) {
     return arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 }
 
-console.log(sum(arr))
+console.log(sum([9, 12, 34, 24, 130]))
 
 // output 
 // 209
